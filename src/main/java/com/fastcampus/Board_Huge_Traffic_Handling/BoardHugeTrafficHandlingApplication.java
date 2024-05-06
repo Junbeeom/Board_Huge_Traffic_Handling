417d94e4c70317bfbcf73b9e385c3959d31158f9
jojunbeom <wns_p@naver.com>
2024-05-06 13:39:28 +0900

    Initial commit

---
+++ src/main/java/com/fastcampus/Board_Huge_Traffic_Handling/BoardHugeTrafficHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.Board_Huge_Traffic_Handling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardHugeTrafficHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardHugeTrafficHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastcampus/Board_Huge_Traffic_Handling/BoardHugeTrafficHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.Board_Huge_Traffic_Handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardHugeTrafficHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
